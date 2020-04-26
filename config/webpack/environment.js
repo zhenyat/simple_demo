26c46f3be2882425bacf4183c4b0a694ffd1437f
Zhenya Telyukov <telyukov@gmail.com>
2020-04-26 11:08:52 +0300

    initial

---
+++ config/webpack/environment.js
@@ -0,0 +1,8 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+}))
+
+module.exports = environment
